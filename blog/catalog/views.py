58834b247d6502b6de88287bc56968a1de71d49b
Admin <6axapeb1992@rambler.ru>
2023-01-24 12:36:44 +0400

    Created our home page: Created the following files: catalog\templates\base_generic.html catalog\templates\index.html catalog\static\styles.css Changed the following files: catalog\urls.py views.py

--- blog/catalog/views.py
+++ blog/catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Blogger, Post, Comment
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_bloggers = Blogger.objects.all().count()
+    num_posts = Post.objects.all().count()
+    num_comments = Comment.objects.all().count()
+
+    context = {
+        'num_bloggers': num_bloggers,
+        'num_posts': num_posts,
+        'num_comments': num_comments,
+    }
+
+    # Render the HTML template index.html with the data in the contex variable
+    return render(request, 'index.html', context=context)
